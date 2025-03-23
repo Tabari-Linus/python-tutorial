2fd282cdb7596718992829610c51375c314080d1
Mr Lii <leetamparega@gmail.com>
2025-03-23 22:47:52 +0000

    inheritance and methods

--- OOP/heritance.py
+++ OOP/heritance.py
@@ -35,10 +35,46 @@ class Employee:
 
 
 class Developer(Employee):
-    pass 
+    
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
 
-dev_1 = Developer("Linus", "Nex", 10000)
-dev_2 = Developer('Mr', "Lii", 8000)
+class Manager(Employee):
+    
+    def __init__(self, first, last, pay, employees = None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+    
 
-print(dev_1.fullname())
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+        
+    
+    def print_emp(self):
+        for emp in self.employees:
+            print("--->", emp.fullname())
+
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+
+
+
+dev_1 = Developer("Linus", "Nex", 10000, "Python")
+dev_2 = Developer('Mr', "Lii", 8000, "Java")
+
+mgr_1 = Manager("Sue", "Smith", 9000, [dev_1])
+
+print(mgr_1.email)
+print(mgr_1.print_emp())
+mgr_1.remove_emp(dev_1)
+mgr_1.add_emp(dev_2)
+print(mgr_1.print_emp())
